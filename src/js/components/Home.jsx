9f6dd3d8f58608e113c4150cd24fef9a51dab657
JuanoBQ <juanjo.bravo3@gmail.com>
2025-02-24 15:21:22 -0500

    traffic-light

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./Home.css";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+  const [color, setColor] = useState("");
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  useEffect(() => {
+    const auto = setInterval(() => {
+      setColor((prevColor) => {
+        if (prevColor === "selected red-light") return "selected orange-light";
+        if (prevColor === "selected orange-light")
+          return "selected green-light";
+        return "selected red-light";
+      });
+    }, 2000);
+    return () => clearInterval(auto);
+  }, [color]);
+
+  return (
+    <div className="container">
+      <div className="traffic-top">
+        <div className="stick"></div>
+      </div>
+      <div className="traffic-ligth">
+        <div
+          className={
+            "red" + " " + (color === "red" ? "selected red-light" : "")
+          }
+          onClick={() => setColor("red")}
+        ></div>
+        <div
+          className={
+            "orange" + " " + (color === "orange" ? "selected yellow-light" : "")
+          }
+          onClick={() => setColor("orange")}
+        ></div>
+        <div
+          className={
+            "green" + " " + (color === "green" ? "selected green-light" : "")
+          }
+          onClick={() => setColor("green")}
+        ></div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
